10b932c4a39893e21b5821a5490c152271205fda
Anshika <anshibiz126@gmail.com>
2025-11-03 18:57:15 +0530

    basename added for github pages deploy

--- src/index.js
+++ src/index.js
@@ -1,38 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { createBrowserRouter,RouterProvider } from "react-router-dom";
-import Login from './pages/Login';
-import Home from './pages/Home';
-import SignUp from './pages/SignUp';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
 
-const appRouter = createBrowserRouter([
+const appRouter = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          path: "/",
+          element: <SignUp />,
+        },
+        {
+          path: "/login",
+          element: <Login />,
+        },
+        {
+          path: "/home",
+          element: <Home />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
-    children:[
-      {
-        path: "/",
-        element: <SignUp/>
-      },
-      {
-        path: "/login",
-        element: <Login/>
-      },
-      {
-        path: "/home",
-        element: <Home/>
-      }
-    ]
+    // for GitHub Pages deployment
+    basename: "/Assignment_ArubaNetworks",
   }
-])
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-      <RouterProvider router={appRouter} />
-  </React.StrictMode>
 );
 
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={appRouter} />
+  </React.StrictMode>
+);
